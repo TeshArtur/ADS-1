6957ebb453835dad85a93c0f02a0c3da46594fff
Артур Тэш <79190924+TeshArtur@users.noreply.github.com>
2022-02-27 22:41:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,31 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+#include <iostream>
+using namespace std;
+int main() {
+    int num = 0;
+    int i = 2;
+    int Otv = 0;
+    float Z = 0;
+    cin >> num;
+
+    while (i < num)
+    {
+        Z = num % i;      //cout << num << "|" << i << endl;
+        if (Z == 0) {
+            Otv = 1;
+            break;
+        }
+        i++;
+    }
+    if (Otv == 1) {
+        cout << "false" << endl;
+    }
+    else {
+        cout << "true" << endl;
+    }
+}
 }
 
 uint64_t nPrime(uint64_t n) {
